c5d1db3961f0a97321407a5939c2b0414b1eeb4e
bogdan-stv <32815685+bogdan-stv@users.noreply.github.com>
2021-07-24 21:15:45 +0300

    Update homework_01.py

--- Homework/homework_01.py
+++ Homework/homework_01.py
@@ -1,26 +1,39 @@
 """
 Create 2 decorators that will get applied to the function below
-    1) first decorator applied insures that input arguments and output values are converted to the arguments provided as decorator
-    2) second decorator will write each output values on a new line in a file that matches the function name
-    Use functions created by you
+1) first decorator applied insures that input arguments and output values are converted to the arguments provided as decorator
+2) second decorator will write each output values on a new line in a file that matches the function name
+Use functions created by you
 """
 
 from functools import wraps
 
-def greeting(*args1):
+def check_args(*args1):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            with open(f"{func.__name__}.txt", 'w') as file:
-                for arg in args1:
-                    file.write(f"{arg}\n")
+            args = list(args)
+            for i in range(len(args)):
+                if type(args[i]) != args1[i]:
+                    args[i] = args1[i](args[i])
             result = func(*args, **kwargs)
             return result
         return wrapper
     return decorator
 
-@greeting("Hello", "World")
-def introduction(name):
-    print(f"My name is {name}")
+def write_args(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        with open(f"{func.__name__}.txt", 'w') as file:
+            for arg in args:
+                file.write(f"{arg}\n")
+        result = func(*args, **kwargs)
+        return result
+    return wrapper
 
-introduction("Bogdan")
+@check_args(str, str)
+@write_args
+def function(arg1, arg2):
+    print(arg1, arg2)
+
+function("Hello", "World")
+
